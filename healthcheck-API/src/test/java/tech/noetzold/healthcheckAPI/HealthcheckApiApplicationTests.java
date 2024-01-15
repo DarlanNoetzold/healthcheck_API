a4402d44b6c1181fd76d07fa5bcf83c827438830
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-14 21:50:16 -0300

    Create HealthcheckApiApplicationTests.java

---
+++ healthcheck-API/src/test/java/tech/noetzold/healthcheckAPI/HealthcheckApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.noetzold.healthcheckAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcheckApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
