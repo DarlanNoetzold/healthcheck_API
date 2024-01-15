09fee3f3575c3aa9909711463919f24db5643434
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-14 21:50:09 -0300

    Create HealthcheckApiApplication.java

---
+++ healthcheck-API/src/main/java/tech/noetzold/healthcheckAPI/HealthcheckApiApplication.java
@@ -0,0 +1,13 @@
+package tech.noetzold.healthcheckAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcheckApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcheckApiApplication.class, args);
+	}
+
+}
