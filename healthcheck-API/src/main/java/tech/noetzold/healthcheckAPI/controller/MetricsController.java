8f4cf220b8b9a215cc78e716baa9ed04a6dc8357
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-19 14:41:27 -0300

    Update MetricsController.java

--- healthcheck-API/src/main/java/tech/noetzold/healthcheckAPI/controller/MetricsController.java
+++ healthcheck-API/src/main/java/tech/noetzold/healthcheckAPI/controller/MetricsController.java
@@ -1,4 +1,32 @@
 package tech.noetzold.healthcheckAPI.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.noetzold.healthcheckAPI.model.MetricResponse;
+import tech.noetzold.healthcheckAPI.repository.MetricsRepository;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api")
 public class MetricsController {
+
+    @Autowired
+    MetricsRepository metricsRepository;
+
+
+    @GetMapping("/ping")
+    public String ping(){
+        return "API is up";
+    }
+
+    @GetMapping("/all")
+    public List<MetricResponse> getAllMetrics(){
+        return metricsRepository.findAll();
+    }
+
 }
