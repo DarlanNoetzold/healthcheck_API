06db7ab2214126ab99606af18b139986ddae5eba
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-19 14:41:21 -0300

    Update MetricResponse.java

--- healthcheck-API/src/main/java/tech/noetzold/healthcheckAPI/model/MetricResponse.java
+++ healthcheck-API/src/main/java/tech/noetzold/healthcheckAPI/model/MetricResponse.java
@@ -1,10 +1,11 @@
 package tech.noetzold.healthcheckAPI.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -19,10 +20,31 @@ public class MetricResponse {
     private String description;
     private String baseUnit;
 
-    @OneToMany(mappedBy = "metricResponse", cascade = CascadeType.ALL)
-    private List<Measurement> measurements;
+    @OneToMany(mappedBy = "metricResponse", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Measurement> measurements = new ArrayList<>();
 
-    @OneToMany(mappedBy = "metricResponse", cascade = CascadeType.ALL)
-    private List<Tag> availableTags;
+    @OneToMany(mappedBy = "metricResponse", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Tag> availableTags = new ArrayList<>();
 
+    public void addMeasurement(Measurement measurement) {
+        measurements.add(measurement);
+        measurement.setMetricResponse(this);
+    }
+
+    public void removeMeasurement(Measurement measurement) {
+        measurements.remove(measurement);
+        measurement.setMetricResponse(null);
+    }
+
+    public void addTag(Tag tag) {
+        availableTags.add(tag);
+        tag.setMetricResponse(this);
+    }
+
+    public void removeTag(Tag tag) {
+        availableTags.remove(tag);
+        tag.setMetricResponse(null);
+    }
 }
