9f17288ded54890b172e912c19609423ce873257
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-21 00:39:23 -0300

    Update MetricsRepository.java

--- healthcheck-API/src/main/java/tech/noetzold/healthcheckAPI/repository/MetricsRepository.java
+++ healthcheck-API/src/main/java/tech/noetzold/healthcheckAPI/repository/MetricsRepository.java
@@ -8,9 +8,4 @@ import tech.noetzold.healthcheckAPI.model.MetricResponseGroupedDTO;
 import java.util.List;
 
 public interface MetricsRepository extends JpaRepository<MetricResponse, Long> {
-    @Query(value = "SELECT name, array_agg(description) AS descriptions " +
-            "FROM metric_response " +
-            "GROUP BY name", nativeQuery = true)
-    List<MetricResponseGroupedDTO> findAllGroupedByName();
-
 }
