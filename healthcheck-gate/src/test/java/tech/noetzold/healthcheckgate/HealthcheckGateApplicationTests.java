313cf461a5f1ddfbed3db5eabc6a918970a0d7be
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-23 08:00:02 -0300

    Create HealthcheckGateApplicationTests.java

---
+++ healthcheck-gate/src/test/java/tech/noetzold/healthcheckgate/HealthcheckGateApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.noetzold.healthcheckgate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcheckGateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
