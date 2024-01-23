98d936ef29bab4d89ff91e47a525dc8ac811250c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-23 07:59:52 -0300

    rename

--- healthcheck-API/src/main/java/tech/noetzold/healthcheckAPI/repository/MeasurementRepository.java
+++
@@ -1,7 +0,0 @@
-package tech.noetzold.healthcheckAPI.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.noetzold.healthcheckAPI.model.Measurement;
-
-public interface MeasurementRepository extends JpaRepository<Measurement, Long> {
-}
--- healthcheck-API/src/main/java/tech/noetzold/healthcheckAPI/repository/TagRepository.java
+++
@@ -1,7 +0,0 @@
-package tech.noetzold.healthcheckAPI.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.noetzold.healthcheckAPI.model.Tag;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
