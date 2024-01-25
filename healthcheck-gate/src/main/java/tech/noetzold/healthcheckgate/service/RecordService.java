62a82811156e50f8a507e3832f9fcf4470b842ab
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-25 08:20:59 -0300

    Update RecordService.java

--- healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/service/RecordService.java
+++ healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/service/RecordService.java
@@ -1,4 +1,13 @@
 package tech.noetzold.healthcheckgate.service;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.noetzold.healthcheckgate.repository.RecordRepository;
+
+@Service
 public class RecordService {
+
+    @Autowired
+    RecordRepository recordRepository;
 }
