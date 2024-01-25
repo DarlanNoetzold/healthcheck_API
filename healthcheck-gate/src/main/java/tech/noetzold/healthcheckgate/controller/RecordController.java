40ed93f8e63a36b64400c3db2585de8a9987562d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-25 08:20:50 -0300

    Update RecordController.java

--- healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/controller/RecordController.java
+++ healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/controller/RecordController.java
@@ -1,4 +1,16 @@
 package tech.noetzold.healthcheckgate.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.noetzold.healthcheckgate.service.RecordService;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/gate")
 public class RecordController {
+
+    @Autowired
+    RecordService recordService;
 }
