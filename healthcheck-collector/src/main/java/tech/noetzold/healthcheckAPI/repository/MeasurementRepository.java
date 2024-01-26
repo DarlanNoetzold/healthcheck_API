becdfc5638f43c10e522c187d5336307b69ec2bd
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-25 21:45:23 -0300

    Update MeasurementRepository.java

--- healthcheck-collector/src/main/java/tech/noetzold/healthcheckAPI/repository/MeasurementRepository.java
+++ healthcheck-collector/src/main/java/tech/noetzold/healthcheckAPI/repository/MeasurementRepository.java
@@ -1,7 +1,9 @@
 package tech.noetzold.healthcheckAPI.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tech.noetzold.healthcheckAPI.model.Measurement;
 
+@Repository
 public interface MeasurementRepository extends JpaRepository<Measurement, Long> {
 }
