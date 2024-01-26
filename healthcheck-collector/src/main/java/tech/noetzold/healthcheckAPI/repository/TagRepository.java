a27af9536fb0739f4d0d59611d8442e346cec9ed
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-25 21:45:27 -0300

    Update TagRepository.java

--- healthcheck-collector/src/main/java/tech/noetzold/healthcheckAPI/repository/TagRepository.java
+++ healthcheck-collector/src/main/java/tech/noetzold/healthcheckAPI/repository/TagRepository.java
@@ -1,7 +1,9 @@
 package tech.noetzold.healthcheckAPI.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tech.noetzold.healthcheckAPI.model.Tag;
 
+@Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
 }
