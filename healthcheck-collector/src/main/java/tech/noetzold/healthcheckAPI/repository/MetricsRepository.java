ffc51fcaf5385cff1bb128c84b22a922c0965ecd
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-25 21:45:25 -0300

    Update MetricsRepository.java

--- healthcheck-collector/src/main/java/tech/noetzold/healthcheckAPI/repository/MetricsRepository.java
+++ healthcheck-collector/src/main/java/tech/noetzold/healthcheckAPI/repository/MetricsRepository.java
@@ -1,11 +1,9 @@
 package tech.noetzold.healthcheckAPI.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import tech.noetzold.healthcheckAPI.model.MetricResponse;
-import tech.noetzold.healthcheckAPI.model.MetricResponseGroupedDTO;
 
-import java.util.List;
-
+@Repository
 public interface MetricsRepository extends JpaRepository<MetricResponse, Long> {
 }
