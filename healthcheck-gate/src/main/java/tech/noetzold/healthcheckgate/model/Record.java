78f0f2d6e4a04c5af6cc162a8ed2480f60eaba62
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-26 21:22:11 -0300

    Update Record.java

--- healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/model/Record.java
+++ healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/model/Record.java
@@ -1,6 +1,9 @@
 package tech.noetzold.healthcheckgate.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,4 +13,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Record {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Status status;
+    private String property;
+    private String value;
 }
