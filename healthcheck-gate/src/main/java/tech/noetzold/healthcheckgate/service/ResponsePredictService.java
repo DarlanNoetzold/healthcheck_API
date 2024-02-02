b46c5fbfd360eba0c3c2b36c533c5521b2601081
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-02-02 09:34:44 -0300

    Update ResponsePredictService.java

--- healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/service/ResponsePredictService.java
+++ healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/service/ResponsePredictService.java
@@ -1,4 +1,12 @@
 package tech.noetzold.healthcheckgate.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.noetzold.healthcheckgate.repository.ResponsePredictRepository;
+
+@Service
 public class ResponsePredictService {
+
+    @Autowired
+    ResponsePredictRepository responsePredictRepository;
 }
