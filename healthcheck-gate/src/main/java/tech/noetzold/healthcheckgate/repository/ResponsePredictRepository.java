7113c6616f6584448206bae2999f9eb987183c6f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-02-02 09:34:42 -0300

    Update ResponsePredictRepository.java

--- healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/repository/ResponsePredictRepository.java
+++ healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/repository/ResponsePredictRepository.java
@@ -1,4 +1,7 @@
 package tech.noetzold.healthcheckgate.repository;
 
-public class ResponsePredictRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.noetzold.healthcheckgate.model.ResponsePredict;
+
+public interface ResponsePredictRepository extends JpaRepository<ResponsePredict, Long> {
 }
