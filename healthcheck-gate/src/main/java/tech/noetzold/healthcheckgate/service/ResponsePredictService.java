772f5c744f80bf3b578d26d564753a9f5e73ed63
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-02-05 08:33:18 -0300

    Update ResponsePredictService.java

--- healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/service/ResponsePredictService.java
+++ healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/service/ResponsePredictService.java
@@ -1,6 +1,8 @@
 package tech.noetzold.healthcheckgate.service;
 
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tech.noetzold.healthcheckgate.model.ResponsePredict;
@@ -15,16 +17,21 @@ public class ResponsePredictService {
     @Autowired
     private ResponsePredictRepository responsePredictRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(ResponsePredictService.class);
+
     @Transactional
     public ResponsePredict createResponsePredict(ResponsePredict responsePredict) {
+        logger.info("Create a ResponsePredict Predict: {}", responsePredict.toString());
         return responsePredictRepository.save(responsePredict);
     }
 
     public List<ResponsePredict> getAllResponsePredicts() {
+        logger.info("Finding ResponsePredicts");
         return responsePredictRepository.findAll();
     }
 
     public Optional<ResponsePredict> getResponsePredictById(Long id) {
+        logger.info("Finding ResponsePredict by id: {}", id);
         return responsePredictRepository.findById(id);
     }
 
@@ -33,6 +40,7 @@ public class ResponsePredictService {
         ResponsePredict responsePredict = responsePredictRepository.findById(id)
                 .orElseThrow(() -> new Exception("ResponsePredict not found for this id :: " + id));
 
+        logger.info("Update ResponsePredict: {}", responsePredictDetails.toString());
         responsePredict.setFutureRecord(responsePredictDetails.getFutureRecord());
         responsePredict.setFutureDate(responsePredictDetails.getFutureDate());
 
@@ -44,6 +52,7 @@ public class ResponsePredictService {
         ResponsePredict responsePredict = responsePredictRepository.findById(id)
                 .orElseThrow(() -> new Exception("ResponsePredict not found for this id :: " + id));
 
+        logger.info("Delete ResponsePredict by id: {}", id);
         responsePredictRepository.delete(responsePredict);
     }
 }
