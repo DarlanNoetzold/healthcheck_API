d145562e7c9fed5ccba9b68a66b9a5add7643418
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-02-12 17:33:04 -0300

    Update extract_dataset_from_api_collector.py

--- extract_dataset_from_api_collector.py
+++ extract_dataset_from_api_collector.py
@@ -13,32 +13,27 @@ metrics_of_interest = [
     "jvm.threads.live", "disk.free", "system.cpu.usage", "process.uptime"
 ]
 
-# Estrutura para acumular os dados coletados, evitando duplicatas
-accumulated_data = {metric: [] for metric in metrics_of_interest}
-last_value = {metric: None for metric in metrics_of_interest}
-
 base_url = "http://192.168.18.75:8194"
-size = 1000
+size = 100
 
-def fetch_data_from_api(page):
+def fetch_data_from_api(session, page):
     url = f"{base_url}/api/all/byname?page={page}&size={size}"
-    response = requests.get(url)
-    if response.status_code == 200:
+    response = session.get(url)
+    if response.status_code == 200 and response.json():
         print(response.json())
         return response.json()
-    else:
-        print(f"Failed to fetch data: {response.status_code}")
-        return None
+    print(response.json())
+    return None
 
-def process_metric_data(metric_data):
-    global accumulated_data, last_value
+def process_metric_data(metric_data, accumulated_data, last_value):
     for metric in metric_data:
         if metric['name'] in metrics_of_interest:
             for response in metric['metricResponses']:
                 if 'measurements' in response:
                     for measurement in response['measurements']:
                         current_value = measurement['value']
-                        if last_value[metric['name']] != current_value:
+                        metric_key = metric['name']
+                        if last_value.get(metric_key, None) != current_value:
                             record = {
                                 "metric_id": response['id'],
                                 "name": response['name'],
@@ -48,38 +43,38 @@ def process_metric_data(metric_data):
                                 "value": current_value,
                                 "tags": "; ".join([f"{tag['tag']}: {', '.join(tag['values'])}" for tag in response.get('availableTags', [])])
                             }
-                            accumulated_data[metric['name']].append(record)
-                            last_value[metric['name']] = current_value
+                            if metric_key not in accumulated_data:
+                                accumulated_data[metric_key] = []
+                            accumulated_data[metric_key].append(record)
+                            last_value[metric_key] = current_value
 
-def save_to_csv(directory):
+def save_to_csv(accumulated_data, directory="metrics_data"):
     if not os.path.exists(directory):
         os.makedirs(directory)
-    for metric, data in accumulated_data.items():
-        if data:
-            df = pd.DataFrame(data)
+    for metric, records in accumulated_data.items():
+        if records:
+            df = pd.DataFrame(records)
             filename = os.path.join(directory, f"{metric.replace('.', '_')}.csv")
             df.to_csv(filename, index=False)
             print(f"Data for metric {metric} saved to {filename}")
 
 def main():
-    directory = "metrics_data"
-    page = 1
-    futures_list = []
-    with ThreadPoolExecutor(max_workers=20000) as executor:
-        while True:
-            future = executor.submit(fetch_data_from_api, page)
-            futures_list.append(future)
-            if future.result() is None:
-                print("No more data available. Stopping.")
-                break
-            page += 1
+    accumulated_data = {metric: [] for metric in metrics_of_interest}
+    last_value = {}
 
-        for future in as_completed(futures_list):
-            data = future.result()
-            if data:
-                process_metric_data(data)
+    with requests.Session() as session:
+        page = 1
+        with ThreadPoolExecutor(max_workers=5000) as executor:
+            while True:
+                future = executor.submit(fetch_data_from_api, session, page)
+                data = future.result()
+                if not data:
+                    break
+                process_metric_data(data, accumulated_data, last_value)
+                page += 1
+                print(str(page))
 
-    save_to_csv(directory)
+    save_to_csv(accumulated_data)
 
 if __name__ == "__main__":
     main()
