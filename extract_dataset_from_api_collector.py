4df3ed9fbd392169e2663243538127d62ba4b241
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-02-12 08:31:18 -0300

    Update extract_dataset_from_api_collector.py

--- extract_dataset_from_api_collector.py
+++ extract_dataset_from_api_collector.py
@@ -1,8 +1,9 @@
 import os
 import requests
 import pandas as pd
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
-# Lista de métricas de interesse selecionadas
+# Lista de métricas de interesse
 metrics_of_interest = [
     "application.ready.time", "application.started.time", "cache.gets",
     "cache.puts", "cache.removals", "hikaricp.connections",
@@ -12,63 +13,72 @@ metrics_of_interest = [
     "jvm.threads.live", "disk.free", "system.cpu.usage", "process.uptime"
 ]
 
-# Estrutura para manter o último valor registrado para evitar duplicatas consecutivas
-last_recorded_values = {metric: None for metric in metrics_of_interest}
+# Estrutura para acumular os dados coletados, evitando duplicatas
+accumulated_data = {metric: [] for metric in metrics_of_interest}
+last_value = {metric: None for metric in metrics_of_interest}
 
-def fetch_data_from_api(base_url, page, size):
+base_url = "http://192.168.18.75:8194"
+size = 100
+
+def fetch_data_from_api(page):
     url = f"{base_url}/api/all/byname?page={page}&size={size}"
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
     else:
-        raise Exception(f"Failed to fetch data: {response.status_code}")
+        print(f"Failed to fetch data: {response.status_code}")
+        return None
 
-def process_metric_responses(metric_responses, accumulated_data):
-    for metric in metric_responses:
-        if metric['name'] in metrics_of_interest:  # Filtra apenas métricas de interesse
+def process_metric_data(metric_data):
+    global accumulated_data, last_value
+    for metric in metric_data:
+        if metric['name'] in metrics_of_interest:
             for response in metric['metricResponses']:
                 if 'measurements' in response:
                     for measurement in response['measurements']:
-                        value = measurement['value']
-                        # Verifica se o registro é uma duplicata consecutiva
-                        if last_recorded_values[metric['name']] != value:
+                        current_value = measurement['value']
+                        if last_value[metric['name']] != current_value:
                             record = {
                                 "metric_id": response['id'],
                                 "name": response['name'],
                                 "description": response.get("description", ""),
                                 "baseUnit": response.get("baseUnit", ""),
                                 "statistic": measurement['statistic'],
-                                "value": value,
+                                "value": current_value,
                                 "tags": "; ".join([f"{tag['tag']}: {', '.join(tag['values'])}" for tag in response.get('availableTags', [])])
                             }
-                            if response['name'] not in accumulated_data:
-                                accumulated_data[response['name']] = []
-                            accumulated_data[response['name']].append(record)
-                            last_recorded_values[metric['name']] = value
-                else:
-                    print(f"Warning: 'measurements' not found in response for metric {response.get('name', 'Unknown')}")
+                            accumulated_data[metric['name']].append(record)
+                            last_value[metric['name']] = current_value
 
-def save_to_csv(accumulated_data, directory):
+def save_to_csv(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
-    for metric_name, data in accumulated_data.items():
-        df = pd.DataFrame(data)
-        filename = os.path.join(directory, f"{metric_name.replace('.', '_')}.csv")
-        df.to_csv(filename, index=False)
-        print(f"Data for metric {metric_name} saved to {filename}")
+    for metric, data in accumulated_data.items():
+        if data:
+            df = pd.DataFrame(data)
+            filename = os.path.join(directory, f"{metric.replace('.', '_')}.csv")
+            df.to_csv(filename, index=False)
+            print(f"Data for metric {metric} saved to {filename}")
 
 def main():
-    base_url = "http://192.168.18.75:8194"
+    directory = "metrics_data"
     page = 1
-    size = 100
-    accumulated_data = {}
-    while True:
-        data = fetch_data_from_api(base_url, page, size)
-        if not data:
-            break
-        process_metric_responses(data, accumulated_data)
-        page += 1
-    save_to_csv(accumulated_data, "metrics_data")
+    futures_list = []
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        while True:
+            future = executor.submit(fetch_data_from_api, page)
+            futures_list.append(future)
+            if future.result() is None:
+                print("No more data available. Stopping.")
+                break
+            page += 1
+
+        for future in as_completed(futures_list):
+            data = future.result()
+            if data:
+                process_metric_data(data)
+
+    save_to_csv(directory)
 
 if __name__ == "__main__":
     main()
