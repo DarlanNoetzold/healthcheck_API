e3eaa96b0de1e5c8b982b7c7d9b24b87d976567e
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-02-18 21:35:13 -0300

    Update model_training.py

--- cython_impl/model_training.py
+++ cython_impl/model_training.py
@@ -10,42 +10,41 @@ from scipy.stats import randint, uniform
 from skopt import BayesSearchCV
 from skopt.space import Integer, Real
 import os
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from data_preparation import prepare_data
 from extract_dataset_from_database import extract
 
+np.int = np.int64
+
 def model_training_evaluation(args):
     metric_name, X_train_scaled, X_test_scaled, y_train, y_test, model_name, mp = args
+    try:
+        model_filename = os.path.join("trained_models", f"{metric_name}_{model_name}.pkl")
 
-    model_filename = os.path.join("trained_models", f"{metric_name}_{model_name}.pkl")
+        if os.path.exists(model_filename):
+            print(f"Modelo {model_name} para {metric_name} já treinado. Pulando...")
+            return
 
-    if os.path.exists(model_filename):
-        print(f"Modelo {model_name} para {metric_name} já treinado. Pulando...")
-        return
+        print(f"Treinando {model_name} para {metric_name}")
 
-    print(f"Treinando {model_name} para {metric_name}")
+        if model_name == 'RandomForestRegressor':
+            search = BayesSearchCV(mp['model'], mp['params'], n_iter=10, cv=3, scoring='neg_mean_squared_error', random_state=42, n_jobs=-1)
+        else:
+            search = RandomizedSearchCV(mp['model'], mp['params'], n_iter=10, cv=3, scoring='neg_mean_squared_error', random_state=42, n_jobs=-1)
 
-    # Usando BayesSearchCV para RandomForestRegressor e RandomizedSearchCV para os outros
-    if model_name == 'RandomForestRegresor':
-        search = BayesSearchCV(mp['model'], mp['params'], n_iter=10, cv=3, scoring='neg_mean_squared_error', random_state=42, n_jobs=-1)
-    else:
-        search = RandomizedSearchCV(mp['model'], mp['params'], n_iter=5, cv=3, scoring='neg_mean_squared_error', random_state=42, n_jobs=-1)
+        search.fit(X_train_scaled, y_train)
+        best_model = search.best_estimator_
 
-    X_train_scaled = X_train_scaled.astype(np.int64)
-    y_train = y_train.astype(np.int64)
+        predictions = best_model.predict(X_test_scaled)
+        mse = mean_squared_error(y_test, predictions)
+        mae = mean_absolute_error(y_test, predictions)
+        r2 = r2_score(y_test, predictions)
+        explained_variance = explained_variance_score(y_test, predictions)
 
-    search.fit(X_train_scaled, y_train)
-    best_model = search.best_estimator_
-
-    predictions = best_model.predict(X_test_scaled)
-    mse = mean_squared_error(y_test, predictions)
-    mae = mean_absolute_error(y_test, predictions)
-    r2 = r2_score(y_test, predictions)
-    explained_variance = explained_variance_score(y_test, predictions)
-
-    print(f"{model_name} - {metric_name} - MSE: {mse}, MAE: {mae}, R2: {r2}, Explained Variance: {explained_variance}")
-    joblib.dump(best_model, model_filename)
-
+        print(f"{model_name} - {metric_name} - MSE: {mse}, MAE: {mae}, R2: {r2}, Explained Variance: {explained_variance}")
+        joblib.dump(best_model, model_filename)
+    except Exception as e:
+        print(f"Erro ao treinar {model_name} para {metric_name}: {e}")
 
 input_dir = "metrics_from_database"
 models_dir = "trained_models"
@@ -58,7 +57,7 @@ models_params = {
         'params': {
             'n_estimators': Integer(5, 50),
             'max_depth': Integer(1, 5),
-            'min_samples_split': Integer(1, 3),
+            'min_samples_split': Integer(2, 10),
             'min_samples_leaf': Integer(1, 3)
         }
     },
@@ -66,14 +65,14 @@ models_params = {
         'model': GradientBoostingRegressor(random_state=42),
         'params': {
             'n_estimators': randint(5, 50),
-            'learning_rate': Real(0.01, 0.1),
-            'max_depth': Integer(1, 4)
+            'learning_rate': uniform(0.01, 0.1),
+            'max_depth': randint(1, 4)
         }
     },
     'SVR': {
         'model': SVR(),
         'params': {
-            'C': Real(0.1, 50),
+            'C': uniform(0.1, 50),
             'gamma': ['scale', 'auto'],
             'kernel': ['linear', 'poly', 'rbf', 'sigmoid']
         }
@@ -103,6 +102,20 @@ def process_metric(filename):
 if __name__ == "__main__":
     extract()
 
+    input_dir = "metrics_from_database"
+    models_dir = "trained_models"
+    if not os.path.exists(models_dir):
+        os.makedirs(models_dir)
+
+    filenames = [f for f in os.listdir(input_dir) if f.endswith(".csv")]
+
     with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
-        filenames = [f for f in os.listdir(input_dir) if f.endswith(".csv")]
-        executor.map(process_metric, filenames)
+        future_to_filename = {executor.submit(process_metric, filename): filename for filename in filenames}
+
+        for future in as_completed(future_to_filename):
+            filename = future_to_filename[future]
+            try:
+                result = future.result()
+                print(f"Processamento concluído para: {filename}")
+            except Exception as exc:
+                print(f"Erro ao processar {filename}: {exc}")
