03d6e0a41bcbb819654f2e477fa8d914e21e892c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-02-19 20:35:31 -0300

    Update predict_next_value.py

--- predict_next_value.py
+++ predict_next_value.py
@@ -2,34 +2,42 @@ from flask import Flask, request, jsonify
 import joblib
 import numpy as np
 import os
+import sklearn.ensemble
 from cython_impl.data_preparation import prepare_data
 
 app = Flask(__name__)
 
+def prepare_data_for_prediction(values, model_input_size):
+    if len(values) < model_input_size:
+        values += [values[-1]] * (model_input_size - len(values))
+    return np.array(values).reshape(1, -1)
+
+
 modelos = {}
-for filename in os.listdir('trained_models'):
+for filename in os.listdir('cython_impl/trained_models'):
     if filename.endswith('.pkl'):
         path = os.path.join('cython_impl/trained_models', filename)
-        modelos[filename.split('_')[0]] = joblib.load(path)
+        modelos[filename] = joblib.load(path)
 
 @app.route('/predict', methods=['POST'])
 def prever():
-    dados = request.get_json()
+    dados = request.json
     name = dados.get('name')
     values = dados.get('values')
 
     if not name or not values:
-        return jsonify({'erro': 'Nome da métrica e valores são necessários.'}), 400
+        return jsonify({'error': 'Nome da métrica e valores são necessários.'}), 400
 
-    X, _ = prepare_data(np.array(values), n=len(values))
-    X = X.reshape(1, -1)
+    model_input_size = 10
+    X = prepare_data_for_prediction(values, model_input_size)
 
     if name in modelos:
         modelo = modelos[name]
         previsao = modelo.predict(X)
-        return jsonify({'previsao': previsao.tolist()}), 200
+        return jsonify({'previsao': previsao[0].tolist()}), 200
     else:
-        return jsonify({'erro': 'Modelo para a métrica especificada não foi encontrado.'}), 404
+        return jsonify({'error': 'Modelo para a métrica especificada não foi encontrado.'}), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
