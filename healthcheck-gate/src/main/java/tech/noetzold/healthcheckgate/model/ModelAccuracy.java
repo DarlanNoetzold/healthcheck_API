6691f437f2bc4fea9f7d925f2bd28b92cdf5e491
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-02-22 20:36:14 -0300

    Create ModelAccuracy.java

---
+++ healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/model/ModelAccuracy.java
@@ -0,0 +1,29 @@
+package tech.noetzold.healthcheckgate.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class ModelAccuracy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String modelName;
+    private String model;
+    private String metricName;
+    private double accuracyValue;
+    private Date trainingDate;
+
+}
+
