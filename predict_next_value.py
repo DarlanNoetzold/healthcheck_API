b5d5f565644ec957d44cf19f5ec53f7289ad3437
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-02-24 11:27:53 -0300

    Update predict_next_value.py

--- predict_next_value.py
+++ predict_next_value.py
@@ -5,66 +5,70 @@ import os
 
 app = Flask(__name__)
 
-def prepare_data_for_prediction_values(values, model_input_size):
+models_dir_first_layer = 'cython_impl/trained_models_first_layer'
+models_dir_second_layer = 'cython_impl/trained_models_second_layer'
+
+model_names_value = ['_GradientBoostingRegressor.pkl', '_RandomForestRegressor.pkl', '_SVR.pkl']
+model_names_alert = ['_GradientBoostingClassifier.pkl', '_LogisticRegression.pkl', '_RandomForestClassifier.pkl']
+
+def prepare_data_for_prediction(values, model_input_size):
     if len(values) < model_input_size:
         values += [values[-1]] * (model_input_size - len(values))
     return np.array(values).reshape(1, -1)
 
-
-def prepare_data_for_prediction_alert(values, n):
+def prepare_data_for_prediction_alert(values, n=10):
     if len(values) < n:
         values += [values[-1]] * (n - len(values))
     elif len(values) > n:
         values = values[-n:]
-    return np.array(values).reshape(1, -n)
-
-
-models_first_layer = {}
-for filename in os.listdir('cython_impl/trained_models_first_layer'):
-    if filename.endswith('.pkl'):
-        path = os.path.join('cython_impl/trained_models_first_layer', filename)
-        models_first_layer[filename] = joblib.load(path)
-
-
-models_second_layer = {}
-for filename in os.listdir('cython_impl/trained_models_second_layer'):
-    if filename.endswith('.pkl'):
-        path = os.path.join('cython_impl/trained_models_second_layer', filename)
-        models_second_layer[filename] = joblib.load(path)
+    return np.array(values).reshape(1, -1)
 
 @app.route('/predict-value', methods=['POST'])
 def predict_value():
-    dados = request.json
-    name = dados.get('name')
-    values = dados.get('values')
-
-    if not name or not values:
-        return jsonify({'error': 'Nome da métrica e valores são necessários.'}), 400
-
+    data = request.json
+    metric_name = data.get('name')
+    values = data.get('values')
     model_input_size = 10
-    X = prepare_data_for_prediction_values(values, model_input_size)
+    X = prepare_data_for_prediction(values, model_input_size)
 
-    if name in models_first_layer:
-        modelo = models_first_layer[name]
-        previsao = modelo.predict(X)
-        return jsonify({'predict': previsao[0].tolist()}), 200
+    predictions = {}
+    for model_name_suffix in model_names_value:
+        model_filename = f'{metric_name}{model_name_suffix}'
+        model_path = os.path.join(models_dir_first_layer, model_filename)
+        if os.path.exists(model_path):
+            model = joblib.load(model_path)
+            prediction = model.predict(X)
+            formatted_model_name = model_name_suffix.lstrip('_').replace('.pkl', '')
+            predictions[formatted_model_name] = prediction[0].tolist()
+
+    if predictions:
+        return jsonify(predictions), 200
     else:
-        return jsonify({'error': 'Modelo para a métrica especificada não foi encontrado.'}), 404
+        return jsonify({'error': 'Nenhum modelo encontrado para a métrica especificada.'}), 404
 
 @app.route('/predict-alert', methods=['POST'])
 def predict_alert():
     data = request.json
-    name = data.get('name')
-    values = data.get('values')
+    metric_name = data.get('name')
+    value = data.get('values')
+    values = [value]
+    X = prepare_data_for_prediction_alert(values, 10)
 
-    if name in models_second_layer:
-        model = models_second_layer[name]
-        X = prepare_data_for_prediction_alert(values, 1)
-        prediction = model.predict(X)
-        is_alert = bool(prediction[0])
-        return jsonify({'is_alert': is_alert}), 200
+    predictions = {}
+    for model_name_suffix in model_names_alert:
+        model_filename = f'{metric_name}{model_name_suffix}'
+        model_path = os.path.join(models_dir_second_layer, model_filename)
+        if os.path.exists(model_path):
+            model = joblib.load(model_path)
+            prediction = model.predict(X)
+            is_alert = bool(prediction[0])
+            formatted_model_name = model_name_suffix.lstrip('_').replace('.pkl', '')
+            predictions[formatted_model_name] = is_alert
+
+    if predictions:
+        return jsonify(predictions), 200
     else:
-        return jsonify({'error': 'Modelo não encontrado para a métrica especificada.'}), 404
+        return jsonify({'error': 'Nenhum modelo encontrado para a métrica especificada.'}), 404
 
 
 if __name__ == '__main__':
