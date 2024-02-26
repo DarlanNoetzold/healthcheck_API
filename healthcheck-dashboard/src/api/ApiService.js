dc2c6cdc813ca67c653ce25bab4d07230f62d373
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-02-26 17:44:52 -0300

    Update ApiService.js

--- healthcheck-dashboard/src/api/ApiService.js
+++ healthcheck-dashboard/src/api/ApiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/gate';
+const API_BASE_URL = 'http://localhost:8199/gate';
 const PREDICT_API_BASE_URL = 'http://localhost:5000';
 
 export const ApiService = {
