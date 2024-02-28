f5a5f837f3222d4888f7f2d32b64881d30b811eb
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-02-28 09:13:14 -0300

    Update ModelAccuracy.java

--- healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/model/ModelAccuracy.java
+++ healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/model/ModelAccuracy.java
@@ -1,9 +1,6 @@
 package tech.noetzold.healthcheckgate.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +11,9 @@ import java.util.Date;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "model_accuracy", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"modelName", "metricName"})
+})
 public class ModelAccuracy {
 
     @Id
