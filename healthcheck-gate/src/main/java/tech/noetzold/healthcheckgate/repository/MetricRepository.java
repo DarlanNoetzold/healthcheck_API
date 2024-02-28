5c9959e90d715ff011ee171f45802bee8aaf1306
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-02-28 09:13:17 -0300

    Update MetricRepository.java

--- healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/repository/MetricRepository.java
+++ healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/repository/MetricRepository.java
@@ -3,5 +3,9 @@ package tech.noetzold.healthcheckgate.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.noetzold.healthcheckgate.model.Metric;
 
+import java.util.Optional;
+
 public interface MetricRepository extends JpaRepository<Metric, Long> {
+
+    Optional<Metric> findByName(String name);
 }
