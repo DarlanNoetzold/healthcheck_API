0acfa810e49903f341b85c7c23d1f5a32d12f8c9
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-02-28 09:13:19 -0300

    Update ModelAccuracyRepository.java

--- healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/repository/ModelAccuracyRepository.java
+++ healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/repository/ModelAccuracyRepository.java
@@ -3,5 +3,9 @@ package tech.noetzold.healthcheckgate.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.noetzold.healthcheckgate.model.ModelAccuracy;
 
+import java.util.Optional;
+
 public interface ModelAccuracyRepository extends JpaRepository<ModelAccuracy, Long> {
+    Optional<ModelAccuracy> findByModelNameAndMetricName(String modelName, String metricName);
+
 }
