b76506c5b396d6d0f8107bef7c5a3892f747af29
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-03-04 15:57:47 -0300

    Create AuthenticationController.java

---
+++ healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/security/controller/AuthenticationController.java
@@ -0,0 +1,52 @@
+package tech.noetzold.healthcheckgate.security.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.noetzold.healthcheckgate.security.user.AuthenticationRequest;
+import tech.noetzold.healthcheckgate.security.user.AuthenticationResponse;
+import tech.noetzold.healthcheckgate.security.user.RegisterRequest;
+import tech.noetzold.healthcheckgate.security.service.AuthenticationService;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/healthcheck/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        logger.info("Doing Login.");
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @PostMapping("/refresh-token")
+    public void refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        service.refreshToken(request, response);
+    }
+
+
+}
