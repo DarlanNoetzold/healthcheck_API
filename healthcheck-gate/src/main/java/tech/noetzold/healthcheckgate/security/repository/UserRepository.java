0ed991dc98c4374a40c237a55dc8b68d871fa3ea
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-03-04 15:57:37 -0300

    Create UserRepository.java

---
+++ healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/security/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package tech.noetzold.healthcheckgate.security.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tech.noetzold.healthcheckgate.security.user.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
+}
