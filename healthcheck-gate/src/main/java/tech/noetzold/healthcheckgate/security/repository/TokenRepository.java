714f5553667b47b558789437c7f6555709f7fdd6
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-03-04 15:57:39 -0300

    Create TokenRepository.java

---
+++ healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/security/repository/TokenRepository.java
@@ -0,0 +1,25 @@
+package tech.noetzold.healthcheckgate.security.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import tech.noetzold.healthcheckgate.security.token.Token;
+import tech.noetzold.healthcheckgate.security.user.User;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+
+    @Query(value = """
+      select t from Token t inner join User u\s
+      on t.user.id = u.id\s
+      where u.id = :id and (t.expired = false or t.revoked = false)\s
+      """)
+    List<Token> findAllValidTokenByUser(Integer id);
+
+    Optional<Token> findByToken(String token);
+
+    Optional<Token> findByUserAndToken(User user, String token);
+
+}
