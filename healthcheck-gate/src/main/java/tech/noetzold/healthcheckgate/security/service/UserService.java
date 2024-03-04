83caf115a85568410a2603879773d260c845982c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-03-04 15:57:29 -0300

    Create UserService.java

---
+++ healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/security/service/UserService.java
@@ -0,0 +1,35 @@
+package tech.noetzold.healthcheckgate.security.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import tech.noetzold.healthcheckgate.security.user.ChangePasswordRequest;
+import tech.noetzold.healthcheckgate.security.user.User;
+import tech.noetzold.healthcheckgate.security.repository.UserRepository;
+
+import java.security.Principal;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository repository;
+    public void changePassword(ChangePasswordRequest request, Principal connectedUser) {
+
+        var user = (User) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
+
+        if (!passwordEncoder.matches(request.getCurrentPassword(), user.getPassword())) {
+            throw new IllegalStateException("Wrong password");
+        }
+
+        if (!request.getNewPassword().equals(request.getConfirmationPassword())) {
+            throw new IllegalStateException("Password are not the same");
+        }
+
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+
+        repository.save(user);
+    }
+}
