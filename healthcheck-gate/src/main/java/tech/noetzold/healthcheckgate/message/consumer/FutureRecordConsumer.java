c7cd422bf37ac80dc2194fa8e0051865aedcfedc
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-03-05 11:51:13 -0300

    Create FutureRecordConsumer.java

---
+++ healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/message/consumer/FutureRecordConsumer.java
@@ -0,0 +1,38 @@
+package tech.noetzold.healthcheckgate.message.consumer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import tech.noetzold.healthcheckgate.controller.RecordController;
+import tech.noetzold.healthcheckgate.message.config.RabbitmqQueues;
+import tech.noetzold.healthcheckgate.model.FutureRecord;
+import tech.noetzold.healthcheckgate.service.RecordService;
+
+@Component
+public class FutureRecordConsumer {
+
+    @Autowired
+    RecordService recordService;
+
+    private static final Logger logger = LoggerFactory.getLogger(RecordController.class);
+
+    @Transactional
+    @RabbitListener(queues = RabbitmqQueues.RECORD_QUEUE)
+    public void consumerRecord(String message) throws JsonProcessingException {
+        FutureRecord record = new ObjectMapper().readValue(message, FutureRecord.class);
+        try {
+            recordService.createRecord(record);
+            logger.info("Consume record - " + record.toString());
+        }catch (Exception ex){
+            logger.error("Error to consume create message for record - " + record.toString(), ex);
+            throw new AmqpRejectAndDontRequeueException("Ops, an error! Message should go to DLQ");
+        }
+
+    }
+}
