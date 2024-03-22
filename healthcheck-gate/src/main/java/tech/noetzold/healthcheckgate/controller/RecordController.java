e60eb015b39ad5a7766848eaa6324bc78fd14e73
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-03-22 11:19:06 -0300

    Update RecordController.java

--- healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/controller/RecordController.java
+++ healthcheck-gate/src/main/java/tech/noetzold/healthcheckgate/controller/RecordController.java
@@ -1,5 +1,7 @@
 package tech.noetzold.healthcheckgate.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,34 +26,46 @@ public class RecordController {
     @Autowired
     private RabbitmqService rabbitmqService;
 
+    private static final Logger logger = LoggerFactory.getLogger(RecordController.class);
+
     @PostMapping("/records")
     public ResponseEntity<FutureRecord> createRecord(@RequestBody FutureRecord futureRecord) {
+        logger.info("Creating a new record: {}", futureRecord);
         rabbitmqService.sendMessage(RabbitmqQueues.RECORD_QUEUE, futureRecord);
-        return new ResponseEntity<FutureRecord>(futureRecord, HttpStatus.CREATED);
+        logger.info("Record created successfully: {}", futureRecord);
+        return new ResponseEntity<>(futureRecord, HttpStatus.CREATED);
     }
 
     @GetMapping("/records")
     public List<FutureRecord> getAllRecords() {
+        logger.info("Fetching all records");
         return recordService.getAllRecords();
     }
 
     @GetMapping("/records/{id}")
     public ResponseEntity<FutureRecord> getRecordById(@PathVariable(value = "id") Long recordId) throws Exception {
+        logger.info("Fetching record by id: {}", recordId);
         FutureRecord futureRecord = recordService.getRecordById(recordId)
                 .orElseThrow(() -> new Exception("FutureRecord not found for this id :: " + recordId));
+        logger.info("Record found: {}", futureRecord);
         return ResponseEntity.ok().body(futureRecord);
     }
 
     @PutMapping("/records/{id}")
     public ResponseEntity<FutureRecord> updateRecord(@PathVariable(value = "id") Long recordId,
                                                      @RequestBody FutureRecord futureRecordDetails) throws Exception {
+        logger.info("Updating record: {} with data: {}", recordId, futureRecordDetails);
         FutureRecord updatedFutureRecord = recordService.updateRecord(recordId, futureRecordDetails);
+        logger.info("Record updated successfully: {}", updatedFutureRecord);
         return ResponseEntity.ok(updatedFutureRecord);
     }
 
     @DeleteMapping("/records/{id}")
     public ResponseEntity<?> deleteRecord(@PathVariable(value = "id") Long recordId) throws Exception {
+        logger.info("Deleting record by id: {}", recordId);
         recordService.deleteRecord(recordId);
+        logger.info("Record deleted successfully: {}", recordId);
         return ResponseEntity.ok().build();
     }
+
 }
