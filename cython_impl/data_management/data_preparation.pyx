3df5a6afc2a3d42d401817431e0454fea013a46f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-04-05 20:26:23 -0300

    Update data_preparation.pyx

--- cython_impl/data_management/data_preparation.pyx
+++ cython_impl/data_management/data_preparation.pyx
@@ -5,7 +5,6 @@ def prepare_data(cnp.ndarray[cnp.float64_t, ndim=1] series, int n=10):
     cdef int i
     cdef list X = []
     cdef list y = []
-    # Verifica se a série tem mais de n elementos
     if series.shape[0] > n:
         for i in range(series.shape[0] - n):
             X.append(series[i:i+n])
